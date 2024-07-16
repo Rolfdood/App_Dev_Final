0190fc896440dbab15fb766949abdac7134442e0
Ralph Clarence C. Bello <82042428+RalphClarence1126@users.noreply.github.com>
2024-07-16 20:42:00 +0800

    Edit Profile Update 7/16/24

--- backend/profile_backend.php
+++ backend/profile_backend.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
-
-// Regex patterns
+//REGEX patterns
 $pattern = [
   'email' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
   'fname' => '/^(?!\d)[a-zA-Z\s]*$/', // Must not start with a digit, allows letters and spaces
+  'mname' => '/^(?!\d)[a-zA-Z\s]*$/', // Must not start with a digit, allows letters and spaces
   'lname' => '/^(?!\d)[a-zA-Z\s]*$/', // Must not start with a digit, allows letters and spaces
   'password' => '/^(?=.*[0-9])(?=.*[\W_]).{8,}$/', // At least 1 number, 1 special character, and at least 8 characters
   'uname' => '/^[a-zA-Z0-9]{1,30}$/', // At most 30 characters long, contains no spaces, and no special characters
 ];
 
-// Error boolean variables
+//Error boolean variables
 $error = [
   'email' => false,
   'uname' => false,
   'fname' => false,
+  'mname' => false,
   'lname' => false,
   'password' => false,
   'dob' => false,
@@ -44,51 +45,71 @@ $email = $row['user_email'];
 $dob = $row['user_dob'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // SAVE
+  // Save actions
   if (isset($_POST['save_uname'])) {
     if (preg_match($pattern['uname'], $_POST['username'])) {
       $_SESSION['user_uname'] = $_POST['username'];
-      $username = $_POST['username'];
-      /* $newquery = "UPDATE user SET user_uname = ? WHERE user_id = ?";
-      $newstmt = mysqli_prepare($db_connect, $newquery);
-      mysqli_stmt_bind_param($stmt, 'ss', $username ,$UID);
-      mysqli_stmt_execute($stmt); */
+      $update_query = "UPDATE user SET user_uname = ? WHERE user_id = ?";
+      $update_stmt = mysqli_prepare($db_connect, $update_query);
+      mysqli_stmt_bind_param($update_stmt, 'ss', $_POST['username'], $UID);
+      mysqli_stmt_execute($update_stmt);
+      unset($_POST['edit_uname']);
+      unset($_SESSION['edit_uname']);
+      $username = $_SESSION['user_uname'];
     } else {
       $error['uname'] = true;
     }
-    unset($_POST['edit_uname']);
   }
   if (isset($_POST['save_fname'])) {
     if (preg_match($pattern['fname'], $_POST['fname'])) {
       $_SESSION['user_fname'] = $_POST['fname'];
-      $fname = $_POST['fname'];
+      $update_query = "UPDATE user SET user_fname = ? WHERE user_id = ?";
+      $update_stmt = mysqli_prepare($db_connect, $update_query);
+      mysqli_stmt_bind_param($update_stmt, 'ss', $_POST['fname'], $UID);
+      mysqli_stmt_execute($update_stmt);
+      unset($_POST['edit_fname']);
+      unset($_SESSION['edit_fname']);
+      $fname = $_SESSION['user_fname'];
     } else {
       $error['fname'] = true;
     }
-    unset($_POST['edit_fname']);
   }
   if (isset($_POST['save_lname'])) {
     if (preg_match($pattern['lname'], $_POST['lname'])) {
       $_SESSION['user_lname'] = $_POST['lname'];
-      $lname = $_POST['lname'];
+      $update_query = "UPDATE user SET user_lname = ? WHERE user_id = ?";
+      $update_stmt = mysqli_prepare($db_connect, $update_query);
+      mysqli_stmt_bind_param($update_stmt, 'ss', $_POST['lname'], $UID);
+      mysqli_stmt_execute($update_stmt);
+      unset($_POST['edit_lname']);
+      unset($_SESSION['edit_lname']);
+      $lname = $_SESSION['user_lname'];
     } else {
       $error['lname'] = true;
     }
-    unset($_POST['edit_lname']);
   }
   if (isset($_POST['save_email'])) {
     if (preg_match($pattern['email'], $_POST['email'])) {
       $_SESSION['user_email'] = $_POST['email'];
-      $email = $_POST['email'];
+      $update_query = "UPDATE user SET user_email = ? WHERE user_id = ?";
+      $update_stmt = mysqli_prepare($db_connect, $update_query);
+      mysqli_stmt_bind_param($update_stmt, 'ss', $_POST['email'], $UID);
+      mysqli_stmt_execute($update_stmt);
+      unset($_POST['edit_email']);
+      unset($_SESSION['edit_email']);
     } else {
       $error['email'] = true;
     }
-    unset($_POST['edit_email']);
   }
   if (isset($_POST['save_dob'])) {
     $_SESSION['user_dob'] = $_POST['dob'];
-    $dob = $_POST['dob'];
+    $update_query = "UPDATE user SET user_dob = ? WHERE user_id = ?";
+    $update_stmt = mysqli_prepare($db_connect, $update_query);
+    mysqli_stmt_bind_param($update_stmt, 'ss', $_POST['dob'], $UID);
+    mysqli_stmt_execute($update_stmt);
     unset($_POST['edit_dob']);
+    unset($_SESSION['edit_dob']);
+    $dob = $_SESSION['user_dob'];
   }
   if (isset($_POST['save_password'])) {
     $new_password = $_POST['new_password'];
@@ -100,41 +121,62 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $update_stmt = mysqli_prepare($db_connect, $update_query);
       mysqli_stmt_bind_param($update_stmt, 'ss', $hashed_password, $UID);
       mysqli_stmt_execute($update_stmt);
+      unset($_POST['edit_password']);
+      unset($_SESSION['edit_password']);
     } else {
-      $error['password'] = true;
-    } 
-    unset($_POST['edit_password']);
+      $_SESSION['password_change_error'] = "Passwords do not match or do not meet criteria.";
+    }
   }
 
-  // EDIT
+  // Edit actions
   if (isset($_POST['edit_uname'])) {
     $_SESSION['edit_uname'] = true;
-  } elseif (isset($_POST['cancel_uname'])) {
+  } else {
     unset($_SESSION['edit_uname']);
   }
   if (isset($_POST['edit_fname'])) {
     $_SESSION['edit_fname'] = true;
-  } elseif (isset($_POST['cancel_fname'])) {
+  } else {
     unset($_SESSION['edit_fname']);
   }
   if (isset($_POST['edit_lname'])) {
     $_SESSION['edit_lname'] = true;
-  } elseif (isset($_POST['cancel_lname'])) {
+  } else {
     unset($_SESSION['edit_lname']);
   }
   if (isset($_POST['edit_email'])) {
     $_SESSION['edit_email'] = true;
-  } elseif (isset($_POST['cancel_email'])) {
+  } else {
     unset($_SESSION['edit_email']);
   }
   if (isset($_POST['edit_dob'])) {
     $_SESSION['edit_dob'] = true;
-  } elseif (isset($_POST['cancel_dob'])) {
+  } else {
     unset($_SESSION['edit_dob']);
   }
   if (isset($_POST['edit_password'])) {
     $_SESSION['edit_password'] = true;
-  } elseif (isset($_POST['cancel_password'])) {
+  } else {
+    unset($_SESSION['edit_password']);
+  }
+
+  // Cancel actions
+  if (isset($_POST['cancel_uname'])) {
+    unset($_SESSION['edit_uname']);
+  }
+  if (isset($_POST['cancel_fname'])) {
+    unset($_SESSION['edit_fname']);
+  }
+  if (isset($_POST['cancel_lname'])) {
+    unset($_SESSION['edit_lname']);
+  }
+  if (isset($_POST['cancel_email'])) {
+    unset($_SESSION['edit_email']);
+  }
+  if (isset($_POST['cancel_dob'])) {
+    unset($_SESSION['edit_dob']);
+  }
+  if (isset($_POST['cancel_password'])) {
     unset($_SESSION['edit_password']);
   }
 }
